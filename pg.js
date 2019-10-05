47d95ab1f6b23265c43838047d1b57088f7aec28
Joseph Ledbetter <josephb.ledbetter@gmail.com>
2019-10-04 20:35:47 -0400

    first variables connecting to the HTML

---
+++ pg.js
@@ -0,0 +1,11 @@
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
+
+// turning html into objects//
+var wins = 0;
+var losses = 0;
+var left = 9;
+var guesses = 9;
+var guessesSoFar = [];
+var psychicLetter;
+var answer = "j";
+
